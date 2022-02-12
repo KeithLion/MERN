1d6d64061546c14fd3608a5080b45330e6e03c8c
KeithLion <nortonkiara6@gmail.com>
2022-02-12 13:03:33 -0800

    changed the main page and added hmtl

--- React/first-project/src/App.js
+++ React/first-project/src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-list">
+<h1>Hello World</h1>
+<h3>Things I need to do:</h3>
+<ul>
+  <li>Learn React</li>
+  <li>Feed the dogs</li>
+  <li>go to work</li>
+</ul>
       </header>
     </div>
   );
