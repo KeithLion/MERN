05ba535f9595967f6d78b84ef1b1108934f2cb2c
KeithLion <nortonkiara6@gmail.com>
2022-02-15 10:55:30 -0800

    Person card assignment, created with react app. started creating component

---
+++ React/prop-practice/src/components/PersonCard.js
@@ -0,0 +1,11 @@
+import React from React;
+
+const card =(props) => {
+    return(
+        <div>
+            <h1> first-name, last-name</h1>
+            <p>Age: </p>
+            <p>Hair Color: </p>
+        </div>
+    )
+    }
