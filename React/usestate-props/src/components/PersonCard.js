1cfdc0bc6e4871845288a3442b42d98a2323daa5
KeithLion <nortonkiara6@gmail.com>
2022-02-16 10:49:21 -0800

    cleaned up the button function

--- React/usestate-props/src/components/PersonCard.js
+++ React/usestate-props/src/components/PersonCard.js
@@ -2,12 +2,13 @@ import React ,{useState }from 'react';
 
 const Card =(props) => {
     const [age, setAge] = useState(props.age);
+    const handleClick = () => setAge(age + 1)
     return(
         <div>
             <h1> {props.lastName}, {props.firstName}</h1>
             <p>Age: { age } </p>
             <p>Hair Color: { props.hairColor } </p>
-            <button onClick={(event) => setAge(age + 1)}> Birthday button for {props.firstName}
+            <button onClick = {handleClick}> Birthday button for {props.firstName}
 
             </button>
         </div>
