8d0ae82eac26a1be6e60b3a632da7dcaeb953a52
KeithLion <nortonkiara6@gmail.com>
2022-02-27 20:54:45 -0800

    started adding the functions of the todo app

--- React/todo-list/src/App.js
+++ React/todo-list/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+  const {newTask,setNewTask} = useState('')
+  const {task,setTask} = useState([])
+  const handleNewTask = (e)=>{
+    e.preventDefault()
+    console.log(newTask)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+      <form onSubmit={(e) =>{
+        handleNewTask(e)
+      }}>
+
+      <input 
+      onChange={(e) => {
+        setNewTask(e.target.value);
+      }}
+      type='text'
+      />
+      <div>
+      <button>Add</button>
+      </div>
+      </form>
+      </div>
+    
   );
 }
 
