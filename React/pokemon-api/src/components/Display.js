60da24056a96bd6324325dd7880a65a56ffb06c6
KeithLion <nortonkiara6@gmail.com>
2022-03-04 12:40:18 -0800

    created a pokemon list using an api

--- React/pokemon-api/src/components/Display.js
+++ React/pokemon-api/src/components/Display.js
@@ -1,11 +1,25 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
 
 
 const Display = (props) =>{
+    const {pokemonList,setPokemonList} = useState(null);
+    useEffect(()=> {
+        axios.get('https://pokeapi.co/api/v2/pokemon?=limit=807')
+        .then(response=>{setPokemonList(response.data.results)})
+    },[]);
+
 
     return(
         <div>
-            Display All
+            <h1>Pokemon</h1>
+                <ul>
+                    {pokemonList.map((pokemon,index)=> (
+                        <li key={index}>
+                            {pokemon.name}
+                        </li>
+                        ))}
+                </ul>
         </div>
     )
 }
