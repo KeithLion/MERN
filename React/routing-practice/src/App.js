0389894cf02f72ca00b9579099888bf31d2c429f
KeithLion <nortonkiara6@gmail.com>
2022-03-05 08:47:19 -0800

    initiated a new react project

---
+++ React/routing-practice/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
