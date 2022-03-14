239b5be40c3ed61a4bf5e596fd44e3ac92432aa0
KeithLion <nortonkiara6@gmail.com>
2022-03-13 23:00:46 -0700

    updated the fake commands

--- React/faker-api/src/server.js
+++ React/faker-api/src/server.js
@@ -7,18 +7,20 @@ const createUser = () =>({
       _id: faker.datatype.id(),
       firstName: faker.name.firstName(),
       lastName:faker.name.lastName(),
-      phoneNumber:faker.commerce.phoneNumber(),
+      phoneNumber:faker.phone.phoneNumber(),
       email:faker.commerce.email(),
       password:faker.commerce.password()
 })
 
-const createCompany = () => {
-  const company = {
+const createCompany = () => ({
     _id:faker.commerce._id(),
     name:faker.commerce.name(),
     address:{
     street: faker.address.street(),
-
+    city: faker.city(),
+    state:faker.state(),
+    zipCode:faker.zip(),
+    country:faker.country()
   };
-  return company;
-}
+
+})
