192a3445561480740547dc8be16e2a7466206cdb
KeithLion <nortonkiara6@gmail.com>
2022-03-17 21:40:34 -0700

    moved the routes around

--- MongoDB/Jokes/server/routes/jokes.route.js
+++ MongoDB/Jokes/server/routes/jokes.route.js
@@ -1,9 +1,9 @@
 const JokeController = require('../controllers/jokes.controller')
 
-module.exports = app => {
+module.exports = (app) => {
     app.get('/api/jokes', JokeController.findAllJokes)
     app.post('/api/jokes', JokeController.createNewJoke)
-    app.put('/api/jokes/:_id',JokeController.updateJoke)
     app.get('/api/jokes/:_id',JokeController.findOneJoke)
+    app.put('/api/jokes/:_id',JokeController.updateJoke)
     app.delete('/api/jokes/:_id', JokeController.removeJoke)
 }
