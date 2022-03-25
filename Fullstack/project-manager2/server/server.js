74459afea5e96ff040e67d7da08e84eff9ef31c8
KeithLion <nortonkiara6@gmail.com>
2022-03-24 18:43:33 -0700

    added the backend to the project

---
+++ Fullstack/project-manager2/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 8000
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use(cors())
+require('./config/mongoose.config')
+require('./routes/products.routes')(app);
+app.listen(port,() => console.log(`listening on port: ${port}`))
